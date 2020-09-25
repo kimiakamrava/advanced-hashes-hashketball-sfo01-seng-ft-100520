dda38b8c566aa4d778a452bdb945bcdd58912534
kimia kamrava <kimiakamrava93@gmail.com>
2020-09-25 20:07:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -129,13 +129,12 @@ end
  
 
 
-#def all_players
-  #game_hash[:home][:players].concat (game_hash[:away][:players])
-  
-#end
+def all_players
+  game_hash[:home][:players].concat (game_hash[:away][:players])
+  end
 
-#def num_points_scored (player_name)
- # all_players[:player_name][:points]
+def num_points_scored (player_name)
+ all_players[1][:points]
 
 
 
